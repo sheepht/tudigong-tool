98dbfacbf88bce231b56d4b6f4654567ad9ad23d
sexyoung <Habuche@gmail.com>
2024-08-15 16:10:29 +0800

    feat: append prefix 'dev' to compose services #2

--- dev/healthcheck-service.sh
+++ dev/healthcheck-service.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 while true; do
-  if wget -q -O- http://service:8080/health > /dev/null 2>&1; then
+  if wget -q -O- http://dev-service:8080/health > /dev/null 2>&1; then
     wget -q -O- "${PING_URL_SERVICE}"
   else
     wget -q -O- "${PING_URL_SERVICE}/fail"
