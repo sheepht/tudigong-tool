c55f251dec87dd185e0726843ce190c2b087b1df
sexyoung <Habuche@gmail.com>
2024-11-21 11:45:42 +0800

    fix: e2e update

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,7 +6,7 @@ dotenv.config(); // 載入 .env 檔案
 export default defineConfig({
   e2e: {
     baseUrl: process.env.CYPRESS_baseUrl,
-    defaultCommandTimeout: 300000,
+    // defaultCommandTimeout: 300000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
