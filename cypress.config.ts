1eb82d6f27411bc4c15d112105ae073fd268b78e
sexyoung <Habuche@gmail.com>
2024-11-26 17:03:05 +0800

    chore: setting only fail to slack channel #11

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "cypress";
+import fs from "fs";
 
 import dotenv from "dotenv";
 dotenv.config(); // 載入 .env 檔案
@@ -9,6 +10,21 @@ export default defineConfig({
     // defaultCommandTimeout: 300000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      on(
+        "after:spec",
+        (spec: Cypress.Spec, results: CypressCommandLine.RunResult) => {
+          if (results && results.video) {
+            // Do we have failures for any retry attempts?
+            const failures = results.tests.some((test) =>
+              test.attempts.some((attempt) => attempt.state === "failed")
+            );
+            if (!failures) {
+              // delete the video if the spec passed and no tests retried
+              fs.unlinkSync(results.video);
+            }
+          }
+        }
+      );
       return config;
     },
     video: true,
